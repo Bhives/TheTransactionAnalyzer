23a68022f41a1a40dfa28a9883d2aaed9397ae8b
Bhives <scroush@yandex.ru>
2020-11-10 13:27:12 +0300

    added output for number of transactions

--- src/main/java/ProgramStart.java
+++ src/main/java/ProgramStart.java
@@ -5,6 +5,6 @@ public class ProgramStart {
     public static void main(String[] args) throws NullPointerException {
         TransactionAnalyser transactionAnalyser = new TransactionAnalyser(new File("F:/GitHub/Java/The_Transaction_Analyser/src/main/resources/transactions.csv"));
         System.out.println(transactionAnalyser.getTransactions().toString());
-
+        System.out.println("Total number of transactions: "+transactionAnalyser.getTransactions().size());
     }
 }
